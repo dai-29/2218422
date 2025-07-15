87884aec9347351b6bb3c65bc43bfdb35428117f
Anubhav Patwal <143015153+dai-29@users.noreply.github.com>
2025-07-15 12:11:01 +0530

    Update RedirectPage.jsx

--- FrontEnd/src/RedirectPage/RedirectPage.jsx
+++ FrontEnd/src/RedirectPage/RedirectPage.jsx
@@ -6,14 +6,11 @@ const RedirectPage = () => {
 
   useEffect(() => {
     const localRedirect = () => {
-      // Try to retrieve from localStorage
       const storedItem = localStorage.getItem(`shortlink_${shortcode}`);
 
       if (storedItem) {
         const parsed = JSON.parse(storedItem);
         const now = new Date().getTime();
-
-        // Check for expiry
         if (!parsed.expiry || now < parsed.expiry) {
           window.location.href = parsed.url;
         } else {
